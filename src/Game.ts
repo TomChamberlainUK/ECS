03f900f980ae2ddfc81454c53365aaad08a276b9
Tom Chamberlain <tom_chamberlain_uk@hotmail.co.uk>
2022-10-30 15:14:24 +0000

    Reworked game loop and updated Game tests

--- src/Game.ts
+++ src/Game.ts
@@ -6,7 +6,7 @@ type ConstructorProps = {
 }
 
 export default class Game {
-  public isRunning: boolean;
+  private isRunning: boolean;
   private currentScene: Scene | null;
   private renderer: Renderer | null;
 
@@ -18,7 +18,7 @@ export default class Game {
 
   start() {
     this.isRunning = true;
-    this.step();
+    this.loopStep();
   }
 
   stop() {
@@ -41,27 +41,8 @@ export default class Game {
     return this.renderer;
   }
 
-  private step() {
-
-    const deltaTime = 1 / 60;
-
-    // Don't run if there's no scene available
-    if (!this.currentScene) {
-      this.stop();
-      return;
-    }
-
-    // Don't run if isRunning is falsey
+  private loopStep() {
     if (!this.isRunning) return;
-
-    // Run all systems
-    for (const system of this.currentScene.systems) {
-      system(this.currentScene.entities, deltaTime);
-    }
-
-    // Recursively call
-    this.step();
-
+    requestAnimationFrame(() => this.loopStep());
   }
-
 }
--- tests/Game.test.ts
+++ tests/Game.test.ts
@@ -4,91 +4,42 @@ import Scene from '~/Scene';
 
 describe('Game', () => {
 
-  it('Should instanciate correctly when passed no arguments', () => {
+  describe('Scene', () => {
+    it('Should allow setting and getting of current scene', () => {
 
-    const game = new Game();
+      const game = new Game();
+      const scene = new Scene();
+  
+      game.setCurrentScene(new Scene());
+  
+      expect(game.getCurrentScene()).toBeInstanceOf(Scene);
+      expect(game.getCurrentScene()).toEqual(scene);
+  
+    });
+  });
 
-    expect(game).toEqual({
-      isRunning: false,
-      currentScene: null,
-      renderer: null
+  describe('Renderer', () => {
+    it('Should allow setting and getting of a renderer', () => {
+
+      const game = new Game();
+      const renderer = new Renderer();
+
+      game.setRenderer(renderer);
+  
+      expect(game.getRenderer()).toBeInstanceOf(Renderer);
+      expect(game.getRenderer()).toEqual(renderer);
+    });
+  });
+
+  describe('The game loop', () => {
+    it('Should start', () => {
+      const spyRequestAnimationFrame = jest.spyOn(window, 'requestAnimationFrame');
+
+      const game = new Game();
+      game.start();
+      expect(spyRequestAnimationFrame).toHaveBeenCalled();
     });
 
+    it.todo('Should stop');
   });
-
-  it('Should instanciate correctly when passed a currentScene argument', () => {
-
-    const game = new Game({
-      currentScene: new Scene
-    });
-
-    expect(game).toEqual({
-      isRunning: false,
-      currentScene: new Scene(),
-      renderer: null
-    });
-
-  });
-
-  it('Can set and get current scene', () => {
-
-    const game = new Game();
-    const scene = new Scene();
-
-    game.setCurrentScene(new Scene());
-
-    expect(game.getCurrentScene()).toEqual(scene);
-
-  });
-
-  it('Should not start if current scene is not set', () => {
-
-    const game = new Game();
-    game.start();
-
-    expect(game.isRunning).toBe(false);
-
-  });
-
-  it('Should be able to start, run systems, and stop', () => {
-
-    const game = new Game();
-    const scene = new Scene();
-
-    // Mock a system to later check if it gets called
-    const mockSystem = jest.fn();
-
-    // Check that the game is running during the loop
-    function testGameIsRunningSystem() {
-      expect(game.isRunning).toBe(true);
-    }
-
-    // Quit the game loop to prevent call stack errors
-    function quitGameSystem() {
-      game.stop();
-    }
-
-    // Add all systems to the scene
-    scene.addSystem(mockSystem);
-    scene.addSystem(testGameIsRunningSystem);
-    scene.addSystem(quitGameSystem);
-
-    // Set the current scene and start game
-    game.setCurrentScene(scene);
-    game.start();
-
-    expect(mockSystem).toHaveBeenCalled();
-    expect(game.isRunning).toBe(false);
-    
-  });
-
-  it('Should allow setting and getting of a renderer', () => {
-    const game = new Game();
-    const renderer = new Renderer();
-    game.setRenderer(renderer);
-
-    expect(game.getRenderer()).toBeInstanceOf(Renderer);
-    expect(game.getRenderer()).toEqual(renderer);
-  })
-
 });
