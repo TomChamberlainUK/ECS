5c4e2cd2340d8966f64b4aff30d20634f31adcd7
Tom Chamberlain <tom_chamberlain_uk@hotmail.co.uk>
2023-01-06 17:13:43 +0000

    Added component assignment and retrieval methods to Entity class

--- src/Entity.ts
+++ src/Entity.ts
@@ -1,8 +1,63 @@
+import type { Component } from './components';
+
+/**
+ * A representation of _something_ in the game engine.
+ * This could be a rock, an enemy spaceship, a player controller etc.
+ * What an entity represents is built through its components. How it behaves is dictated by systems.
+ * 
+ * @see {@link components}
+ * @see {@link systems}
+ */
 export default class Entity {
   public id: number;
+  private components: Record<string, Component>;
 
   constructor() {
     this.id = generateUniqueId();
+    this.components = {};
+  }
+
+  /**
+   * Assigns a specific component and its relevant data to the entity.
+   * @param component The component to be added.
+   */
+  addComponent(component: Component) {
+    this.components[component.name] = component;
+  }
+
+  /**
+   * Unassigns a specific component and its relevant data from the entity.
+   * @param name The indentifying name of the component.
+   */
+  removeComponent(name: string) {
+    delete this.components[name];
+  }
+
+  /**
+   * Checks if a specific component has been assigned to the entity.
+   * @param name The indentifying name of the component.
+   * @returns {boolean}
+   */
+  hasComponent(name: string) {
+    return Boolean(this.components[name]);
+  }
+  
+  /**
+   * Checks if multiple specific components have been assigned to the entity.
+   * @param names The indentifying names of the components.
+   * @returns {boolean}
+   */
+  hasComponents(...names: string[]) {
+    return names.every(name => Boolean(this.components[name]));
+  }
+
+  /**
+   * Gets a specific component that has been assigned to the entity.
+   * @param name The indentifying name of the component.
+   * @returns {Component|undefined} The component if found, or undefined or not.
+   */
+  getComponent(name: string) {
+    return this.components[name];
   }
 }
 
--- tests/Entity.test.ts
+++ tests/Entity.test.ts
@@ -1,10 +1,10 @@
 import Entity from '~/Entity';
+import { Component } from '~/components';
 
 describe('Entity', () => {
 
-  const entity = new Entity();
-
   it('When instantiated an entity should have an ID', () => {
+    const entity = new Entity();
     expect(entity.id).not.toBeUndefined();
   });
 
@@ -14,16 +14,45 @@ describe('Entity', () => {
     let foundRepeatedId = false;
 
     for (let i = 0; i < 10; i++) {
-      const newEntity = new Entity();
-      if (usedIdsMap.has(newEntity.id)) {
+      const entity = new Entity();
+      if (usedIdsMap.has(entity.id)) {
         foundRepeatedId = true;
         break;
       } else {
-        usedIdsMap.set(newEntity.id, true);
+        usedIdsMap.set(entity.id, true);
       }
     }
 
     expect(foundRepeatedId).toBe(false);
+  });
+
+  it('Should be able to be assigned components', () => {
+    const entity = new Entity();
+    entity.addComponent(new Component({ name: 'test' }));
+    expect(entity.hasComponent('test')).toBe(true);
+  });
+
+  it('Should allow users to check if the entity has multiple components', () => {
+    const entity = new Entity();
+    entity.addComponent(new Component({ name: 'testA' }));
+    entity.addComponent(new Component({ name: 'testB' }));
+    entity.addComponent(new Component({ name: 'testC' }));
+    expect(entity.hasComponents('testA', 'testB', 'testC')).toBe(true);
+  });
+
+  it('Should allow assigned components to be retrieved', () => {
+    const entity = new Entity();
+    const testComponent = new Component({ name: 'test' });
+    entity.addComponent(testComponent);
+    const entityComponent = entity.getComponent('test');
+    expect(entityComponent).toBe(testComponent);
+  });
+
+  it('Should be able to have components unassigned', () => {
+    const entity = new Entity();
+    entity.addComponent(new Component({ name: 'test' }));
+    entity.removeComponent('test');
+    expect(entity.hasComponent('test')).toBe(false);
   });
 
 });
