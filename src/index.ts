68a7b1a6ef6a28657c7cf19d289c124f4ce2cb7c
Tom Chamberlain <tom_chamberlain_uk@hotmail.co.uk>
2023-01-07 16:48:30 +0000

    Added Entity class to library exports

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,9 @@ import * as systems from '~/systems/index';
 export { default as Game } from '~/Game';
 export { default as Scene } from '~/Scene';
 export { default as Renderer } from '~/Renderer';
+export { default as Entity } from '~/Entity';
 
 export {
   components,
   systems
-}
+};
