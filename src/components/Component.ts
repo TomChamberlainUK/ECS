46b5f9332ead04b068c93e98001585b041d2a156
Tom Chamberlain <tom_chamberlain_uk@hotmail.co.uk>
2023-01-07 16:52:56 +0000

    Added docs to Component base class

--- src/components/Component.ts
+++ src/components/Component.ts
@@ -2,6 +2,11 @@ type Props = {
   name: string;
 }
 
+/**
+ * Base class for components.
+ * Components are the building blocks of an entity.
+ * They hold the necessary data for representing certain aspects of an entity, such as a sprite or the color of a particle.
+ */
 export class Component {
   public name: string;
 
