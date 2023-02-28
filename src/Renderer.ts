78abadd003fd66405d9686f76d72598774154edf
Tom Chamberlain <tom_chamberlain_uk@hotmail.co.uk>
2023-02-28 17:14:21 +0000

    Added renderCircle method to Renderer

--- src/Renderer.ts
+++ src/Renderer.ts
@@ -1,3 +1,5 @@
+import { IVector2D  } from './maths';
+
 export interface IRenderer {
   canvas?: HTMLCanvasElement
 }
@@ -28,5 +30,41 @@ export default class Renderer {
     const devicePixelRatio = window.devicePixelRatio ?? 1;
     this.canvas.width = this.canvas.scrollWidth * devicePixelRatio;
     this.canvas.height = this.canvas.scrollHeight * devicePixelRatio;
+  }
+
+  renderCircle({ position, rotation, scale, radius, fillColor, strokeColor }: {
+    position: IVector2D,
+    rotation: number,
+    scale: IVector2D,
+    radius: number,
+    fillColor: string | false,
+    strokeColor: string | false
+  }) {
+    this.ctx.save();
+    this.ctx.translate(this.canvas.width / 2, this.canvas.height / 2);
+
+    this.ctx.save();
+    this.ctx.translate(position.x, position.y);
+    this.ctx.rotate(rotation);
+    this.ctx.scale(scale.x, scale.y);
+    this.ctx.beginPath();
+
+    this.ctx.arc(0, 0, radius, 0, Math.PI * 2);
+    this.ctx.closePath();
+
+    if (fillColor) {
+      this.ctx.fillStyle = fillColor;
+      this.ctx.fill();
+    }
+
+    if (strokeColor) {
+      this.ctx.strokeStyle = strokeColor;
+      this.ctx.stroke();
+    }
+
+    this.ctx.restore();
+
+    this.ctx.restore();
+
   }
 }
