30b098cf6dd3e5430ca132b955d31fc77e7248f0
Tom Chamberlain <tom_chamberlain_uk@hotmail.co.uk>
2023-03-05 15:11:38 +0000

    Added tests for renderCircle method

--- tests/core/Renderer.test.ts
+++ tests/core/Renderer.test.ts
@@ -64,4 +64,108 @@ describe('Renderer', () => {
       });
     });
   });
+
+  describe('renderCircle()', () => {
+    describe('When called with both a fillColor and strokeColor', () => {
+      const renderer = new Renderer({ canvas: document.createElement('canvas') });
+
+      jest.spyOn(renderer.canvas, 'scrollWidth', 'get').mockReturnValueOnce(100);
+      jest.spyOn(renderer.canvas, 'scrollHeight', 'get').mockReturnValueOnce(100);
+
+      renderer.resizeCanvas();
+
+      jest.spyOn(renderer.ctx, 'save');
+      jest.spyOn(renderer.ctx, 'restore');
+      jest.spyOn(renderer.ctx, 'translate');
+      jest.spyOn(renderer.ctx, 'rotate');
+      jest.spyOn(renderer.ctx, 'beginPath');
+      jest.spyOn(renderer.ctx, 'closePath');
+      jest.spyOn(renderer.ctx, 'arc');
+      jest.spyOn(renderer.ctx, 'fill');
+      jest.spyOn(renderer.ctx, 'stroke');
+
+      renderer.renderCircle({
+        position: { x: 1, y: 1 },
+        rotation: 0,
+        scale: { x: 1, y: 1 },
+        radius: 10,
+        fillColor: 'white',
+        strokeColor: 'red'
+      });
+
+      it('Should save the current rendering context', () => {
+        expect(renderer.ctx.save).toBeCalled();
+      });
+
+      it('Should begin rendering at the canvas\'s center', () => {
+        expect(renderer.ctx.translate).toBeCalledWith(50, 50);
+      });
+
+      it('Should translate according to the circles position vector', () => {
+        expect(renderer.ctx.translate).toBeCalledWith(1, 1);
+      });
+
+      it('Should rotate according to the circles rotation value', () => {
+        expect(renderer.ctx.rotate).toBeCalledWith(0);
+      });
+
+      it('Should scale according to the circles scale vector', () => {
+        expect(renderer.ctx.scale).toBeCalledWith(1, 1);
+      });
+
+      it('Should draw the path of a circle according to the circle\'s radius', () => {
+        expect(renderer.ctx.beginPath).toBeCalled();
+        expect(renderer.ctx.arc).toBeCalledWith(0, 0, 10, 0, Math.PI * 2);
+        expect(renderer.ctx.closePath).toBeCalled();
+      });
+
+      it('Should fill the path', () => {
+        expect(renderer.ctx.fill).toBeCalled();
+      });
+
+      it('Should apply a stroke to the path', () => {
+        expect(renderer.ctx.stroke).toBeCalled();
+      });
+
+      it('Should restore the original rendering context', () => {
+        expect(renderer.ctx.restore).toBeCalled();
+      });
+    });
+
+    describe('When called without a fillColor', () => {
+      const renderer = new Renderer({ canvas: document.createElement('canvas') });
+
+      jest.spyOn(renderer.ctx, 'fill');
+      
+      renderer.renderCircle({
+        position: { x: 1, y: 1 },
+        rotation: 0,
+        scale: { x: 1, y: 1 },
+        radius: 10,
+        strokeColor: 'red'
+      });
+
+      it('Should not fill the path', () => {
+        expect(renderer.ctx.fill).not.toBeCalled();
+      });
+    });
+
+    describe('When called without a strokeColor', () => {
+      const renderer = new Renderer({ canvas: document.createElement('canvas') });
+
+      jest.spyOn(renderer.ctx, 'stroke');
+      
+      renderer.renderCircle({
+        position: { x: 1, y: 1 },
+        rotation: 0,
+        scale: { x: 1, y: 1 },
+        radius: 10,
+        fillColor: 'white'
+      });
+
+      it('Should not apply a stroke to the path', () => {
+        expect(renderer.ctx.stroke).not.toBeCalled();
+      });
+    });
+  });
 });
