146ee7aa4e02bfd857280868a557f8b9556dd5d9
Tom Chamberlain <tom_chamberlain_uk@hotmail.co.uk>
2023-03-27 12:31:35 +0100

    Added doc comments to Input class

--- src/input/Input.ts
+++ src/input/Input.ts
@@ -1,6 +1,20 @@
-export default class Input {
+/**
+ * Interface for Input.
+ */
+export interface IInput {
+  /** Tracks currently pressed keys */
+  keys: Record<string, boolean>
+}
+
+/**
+ * Class representing an input device used to manipulate the game.
+ */
+export default class Input implements IInput {
   keys: Record<string, boolean>;
 
+  /**
+   * Creates a new Input.
+   */
   constructor() {
     this.keys = {};
   }
