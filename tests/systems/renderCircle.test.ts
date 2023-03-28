8f4ee53250a58d2536419215039eaee078654c6d
Tom Chamberlain <tom_chamberlain_uk@hotmail.co.uk>
2023-03-28 18:41:42 +0100

    Added tests for no renderer being passed to renderCircle system

--- tests/systems/renderCircle.test.ts
+++ tests/systems/renderCircle.test.ts
@@ -4,35 +4,48 @@ import { CRenderCircle, CTransform2D } from '~/components';
 import { renderCircle } from '~/systems';
 
 describe('renderCircle()', () => {
-  describe('When passed an entity with CTransform2D and CRenderCircle components', () => {
+  describe('When passed no renderer', () => {
     const entity = new Entity();
-    entity.addComponent(new CRenderCircle());
-    entity.addComponent(new CTransform2D());
 
-    const canvas = document.createElement('canvas');
-    const renderer = new Renderer({ canvas });
+    jest.spyOn(entity, 'hasComponents');
 
-    it('Should render', () => {
-      jest.spyOn(renderer, 'renderCircle');
+    renderCircle([entity], {});
 
-      renderCircle([entity], { renderer });
-
-      expect(renderer.renderCircle).toHaveBeenCalled();
+    it('Should not iterate over entities', () => {
+      expect(entity.hasComponents).not.toHaveBeenCalled();
     });
   });
 
-  describe('When passed an entity without CTransform2D and CRenderCircle components', () => {
-    const entity = new Entity();
+  describe('When passed a renderer', () => {
+    describe('When passed an entity with CTransform2D and CRenderCircle components', () => {
+      const entity = new Entity();
+      entity.addComponent(new CRenderCircle());
+      entity.addComponent(new CTransform2D());
 
-    const canvas = document.createElement('canvas');
-    const renderer = new Renderer({ canvas });
+      const canvas = document.createElement('canvas');
+      const renderer = new Renderer({ canvas });
 
-    it('Should not render', () => {
+      jest.spyOn(renderer, 'renderCircle');
+  
+      renderCircle([entity], { renderer });
+
+      it('Should render', () => {
+        expect(renderer.renderCircle).toHaveBeenCalled();
+      });
+    });
+
+    describe('When passed an entity without CTransform2D and CRenderCircle components', () => {
+      const entity = new Entity();
+      const canvas = document.createElement('canvas');
+      const renderer = new Renderer({ canvas });
+
       jest.spyOn(renderer, 'renderCircle');
 
       renderCircle([entity], { renderer });
 
-      expect(renderer.renderCircle).not.toHaveBeenCalled();
+      it('Should not render', () => {
+        expect(renderer.renderCircle).not.toHaveBeenCalled();
+      });
     });
   });
 });
